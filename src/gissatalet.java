619bf5fa653b8561c372a9aefb3ba1c2e772f562
Kloska <56342396+Kloska@users.noreply.github.com>
2020-11-17 10:47:17 +0100

    algoritmer träning

---
+++ src/gissatalet.java
@@ -0,0 +1,15 @@
+import javafx.beans.binding.When;
+
+import javax.swing.*;
+
+public class gissatalet {
+    public static void main(String[] args) {
+       double random = Math.random()*100;
+      double input = Integer.parseInt(JOptionPane.showInputDialog("Gissa på ett tal mellan 1-100"));
+
+       if(random == input);
+
+           else
+        JOptionPane.showMessageDialog("Rätt svar!" + "Du gissade rätt efter " + gissningar + "gissningar.");
+    }
+}
