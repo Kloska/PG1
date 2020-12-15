7d5671758cfa66271f92b8bdbef5be5a169ee752
Kloska <56342396+Kloska@users.noreply.github.com>
2020-12-15 11:16:29 +0100

    inlämning - fungerar ej

--- src/Quiz.java
+++ src/Quiz.java
@@ -4,8 +4,9 @@ public class Quiz {
     public static void main(String[] args) {
         String[] choices = { "A - Banan", "B - Apelsin", "C - Äpple"};
         String input = (String) JOptionPane.showInputDialog(null, "Vilken frukt är gul?",
-                "Quiz", JOptionPane.QUESTION_MESSAGE, null, choices, choices[1]);
+                "Quiz",JOptionPane.QUESTION_MESSAGE, null, choices, choices[1]);
         System.out.println(input);
-        "A - Banan".equals(input);
+        if("A - Banan".equals(input));
+        System.out.println("Rätt svar!");
     }
 }
